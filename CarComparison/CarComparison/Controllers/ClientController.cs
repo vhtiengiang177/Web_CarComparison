32637fb9770b5a819a5e8d0e248798e843fef7c8
Thao Nguyen <56422980+Aquas201@users.noreply.github.com>
2020-12-29 16:26:02 +0700

    thêm trang chi tiết

--- CarComparison/CarComparison/Controllers/ClientController.cs
+++ CarComparison/CarComparison/Controllers/ClientController.cs
@@ -38,24 +38,7 @@ namespace CarComparison.Controllers
             return View();
         }
 
-        public ActionResult About()
-        {
-            return View();
-        }
-
-        public ActionResult Team()
-        {
-            return View();
-        }
-
-        
-
-        public ActionResult Contact()
-        {
-            return View();
-        }
-
-        
+        //Truyền dữ liệu xe vào dropdownlist
         public ActionResult Comparing()
         {
             cascadingmodel mol = new cascadingmodel();
@@ -93,70 +76,19 @@ namespace CarComparison.Controllers
 
         }
 
-        //public JsonResult joinName(string id_car)
-        //{
-        //    string name = "";
-        //    var query = (from s in db.Cars
-        //                 join c in db.Versions on s.id_version equals c.id_version
-        //                 join t in db.Models on c.id_model equals t.id_model
-        //                 join h in db.Automakers on t.id_automaker equals h.id_automaker
-                        
-        //                 select new
-        //                 {
-        //                     s.id_car,
-        //                     c.name_version,
-        //                     t.name_model,
-        //                     h.name_automaker
-
-        //                 });
-        //    foreach (var item in query)
-        //    {
-        //        if (item.id_car== id_car)
-        //        {
-        //            name= item.name_automaker + " " + item.name_model + " " + item.name_version;
-        //        }                    
-        //    }
-
-        //    return Json(name, JsonRequestBehavior.AllowGet);
-        //}
-
-
-        public JsonResult joinName(string id_version)
-        {
-            string name = "";
-            var query = (
-                         from c in db.Versions
-                         join t in db.Models on c.id_model equals t.id_model
-                         join h in db.Automakers on t.id_automaker equals h.id_automaker
-                         where id_version == c.id_version
-                         select new
-                         {
-                             c.id_version,
-                             c.name_version,
-                             t.name_model,
-                             h.name_automaker
-
-                         });
-            foreach (var item in query)
-            {
-                if (item.id_version == id_version)
-                {
-                    name = item.name_automaker + " " + item.name_model + " " + item.name_version;
-                }
-            }
-
-            return Json(name, JsonRequestBehavior.AllowGet);
-        }
-
-
         
-
-        
-        
-
+        //Trang bài viết
         public ActionResult Review()
         {
+            //3 bài viết gợi ý
+            var lstnew = (from c in db.Articles where c.id_category != "CaAr01" orderby c.time_pulish_article descending select c).ToList();
+            var lstnew3 = new List<Article>();
+            lstnew3.Add(lstnew[0]);
+            lstnew3.Add(lstnew[1]);
+            lstnew3.Add(lstnew[2]);
+            ViewBag.lstnew3 = lstnew3;
 
+            //Toàn bộ bài viết
             var lstBlog = db.Articles.Where(n => n.id_category != "CaAr01" );
             //Gán vào ViewBag
             ViewBag.ListBlog= lstBlog;
@@ -165,22 +97,50 @@ namespace CarComparison.Controllers
             return View(lstBlog);
         }
 
+        //Chi tiết bài viết
+        public ActionResult DetailBlog(string id)
+        {
+            //3 bài viết gợi ý
+            var lstnew = (from c in db.Articles where c.id_category != "CaAr01" orderby c.time_pulish_article descending select c).ToList();
+
+            var lstnew3 = new List<Article>();
+            lstnew3.Add(lstnew[0]);
+            lstnew3.Add(lstnew[1]);
+            lstnew3.Add(lstnew[2]);
+            ViewBag.lstnew3 = lstnew3;
+
+            //Kiểm tra tham số truyền vào
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            //Nếu không thì truy xuất csdl lấy ra bài viết tương ứng
+            Article art = db.Articles.SingleOrDefault(n=>n.id_article==id && n.id_category!="CaAr01");
+            if (art == null)
+            {
+                //Thông báo không có bài viết đó
+                return HttpNotFound();
+            }
+            return View(art);
+            
+        }
+
+        //Trang video
         public ActionResult Video()
         {
+            //3 video mới nhất gợi ý
+            var lstnew = (from c in db.Articles where c.id_category == "CaAr01" orderby c.time_pulish_article descending select c).ToList();
+            var lstnew3video = new List<Article>();
+            lstnew3video.Add(lstnew[0]);
+            lstnew3video.Add(lstnew[1]);
+            lstnew3video.Add(lstnew[2]);
+            ViewBag.lstnew3video = lstnew3video;
 
-            var videoList = (from a in db.Articles
-                                       where a.id_category == "CaAr01" && a.state_article == "1"
-                                       select a).ToList();
-            Article art = new Article { linkvideo_article = videoList[videoList.Count - 1].linkvideo_article,
-                title_article = videoList[videoList.Count - 1].title_article,
-                time_pulish_article= videoList[videoList.Count - 1].time_pulish_article,
-                id_user = videoList[videoList.Count - 1].id_user
-
-            } ;
-            ViewBag.artLink = art.linkvideo_article;
-            ViewBag.artTitle = art.title_article;
-            ViewBag.artDate = art.time_pulish_article;
-
+            //video lớn ở đầu trang video
+            var videofirst = new List<Article>();
+            videofirst.Add(lstnew[0]);
+            ViewBag.videofirst = videofirst;
+            //Danh sách video
             var lstNewVideo = db.Articles.Where(n => n.id_category == "CaAr01");
             //Gán vào ViewBag
             ViewBag.ListNewVideo = lstNewVideo;
@@ -189,6 +149,33 @@ namespace CarComparison.Controllers
             return View(lstNewVideo);
         }
 
+        //Chi tiết video
+        public ActionResult DetailVideo(string id)
+        {
+            //3 video gợi ý
+            var lstnew = (from c in db.Articles where c.id_category == "CaAr01" orderby c.time_pulish_article descending select c).ToList();
+            var lstnew3video = new List<Article>();
+            lstnew3video.Add(lstnew[0]);
+            lstnew3video.Add(lstnew[1]);
+            lstnew3video.Add(lstnew[2]);
+            ViewBag.lstnew3video = lstnew3video;
+            //Kiểm tra tham số truyền vào
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            //Nếu không thì truy xuất csdl lấy ra bài viết tương ứng
+            Article art = db.Articles.SingleOrDefault(n => n.id_article == id && n.id_category == "CaAr01");
+            if (art == null)
+            {
+                //Thông báo không có bài viết đó
+                return HttpNotFound();
+            }
+            return View(art);
+
+        }
+
+        
         [ChildActionOnly] //để người dùng không get được view này
         public ActionResult blogPartial()
         {
@@ -199,6 +186,24 @@ namespace CarComparison.Controllers
         public ActionResult videoPartial()
         {
             return PartialView();
+        }
+
+        //Trang team
+        public ActionResult Team()
+        {
+            return View();
+        }
+
+
+        //TRang contact
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+        public ActionResult Comment()
+        {
+            return View();
         }
 
 
