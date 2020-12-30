6780484dcdffe99cbf2a9c9164e9fd020ed2cb54
Thao Nguyen <56422980+Aquas201@users.noreply.github.com>
2020-12-30 16:38:05 +0700

    cmmit

---
+++ CarComparison/CarComparison/Areas/Admin/Models/Article_Comment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CarComparison.Areas.Admin.Models
+{
+    public class Article_Comment
+    {
+        public Article ArticleModel { get; set; }
+        public Comment CommentModel { get; set; }
+
+    }
+}
